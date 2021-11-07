78bcb6a63e03a0abd2930bd46268b812313a1db3
Johannes <hruzaj@student.ethz.ch>
2021-11-07 19:50:52 +0100

    bla

--- R/coefficient.R
+++ R/coefficient.R
@@ -1,5 +1,6 @@
+
+
 library(drf)
-library(ggplot2)
 
 multi.which<-function(X,x){
   if (ncol(X)==1) {
@@ -19,33 +20,33 @@ multi.which<-function(X,x){
 # method is by default 'cdf' which is the sampi coefficient, alternatively one can choose 'pdf' which is the mean of l1 difference of the weights.
 #speedupmatrix is a parameter which can be pre-calculated when calling sampi several times with the same X to speed up the calculation such as in variable selection
 
-
-cond.ind.coef <- function(X,Y,Z=NULL, plot=FALSE, method='cdf', speedupmatrix=NULL) {
+#' @export
+cond.ind.coef <- function(X,Y,Z=NULL, method='cdf', speedupmatrix=NULL) {
   X<-as.matrix(X)
   Y<-as.matrix(Y)
   n<-dim(X)[1]
-  
+
   if (!is.null(Z)) {
-    
-    
+
+
     Z<-as.matrix(Z)
-    
+
     if (dim(Y)[1]!= n || dim(Z)[1]!=n) {
       return(warning('X, Y ,Z must be of same length'))
     }
-    
-    
+
+
     if (method=='cdf') {
-      
+
       drf.forest.Z <- drf(Z, X)
-      
+
       drf.forest.Y.Z <- drf(X=cbind(Y,Z), X)
-      
-      
+
+
       w.Z.Y<-get_sample_weights(drf.forest.Y.Z, newdata=cbind(Y,Z))
       w.Z<-get_sample_weights(drf.forest.Z, newdata = Z)
-      
-      
+
+
       if (is.null(speedupmatrix)) {
         speedupmatrix<-matrix(0,n,n)
         for (i in 1:n) {
@@ -57,24 +58,24 @@ cond.ind.coef <- function(X,Y,Z=NULL, plot=FALSE, method='cdf', speedupmatrix=NU
 
       res<-abs(rowSums(as.matrix(w.Z.Y*speedupmatrix))-rowSums(as.matrix(w.Z*speedupmatrix)))
 
-      
-      
 
-      
+
+
+
     }else if (method=='pdf') {
       drf.forest.Z <- drf(Z, X)
-      
+
       drf.forest.Y.Z <- drf(X=cbind(Y,Z), X)
-            
+
       w.Z.Y<-get_sample_weights(drf.forest.Y.Z, newdata=cbind(Y,Z))
       w.Z<-get_sample_weights(drf.forest.Z, newdata = Z)
       res<-rowSums(as.matrix(abs(w.Z.Y-w.Z)))/n
     }else{
       return(warning('wrong method selected'))
     }
-    
+
     res<-mean(res)
-    
+
   }
   else{
     drf.forest.Y <- drf(Y, X)
@@ -84,26 +85,11 @@ cond.ind.coef <- function(X,Y,Z=NULL, plot=FALSE, method='cdf', speedupmatrix=NU
       idx<-multi.which(X,X[j,])
       res[length(res)+1]<-abs(length(idx)/n - sum(w.Z.Y[j,idx]))
     }
-    
-    
-    
+
     res<-mean(res)
-    
-    #################### Visualization: plotting the two CDFs if Z=NULL
-    if (plot) {
-      cdf.1<-vector()
-      cdf.2<-vector()
-      for (j in 1:n) {
-        idx<-multi.which(X,X[j,])
-        cdf.1[length(cdf.1)+1]<-length(idx)/n  
-        cdf.2[length(cdf.2)+1]<-sum(w.Z.Y[j,idx])
-        
-      }
-      print(ggplot()+geom_point(aes(x=X, y = cdf.1, color='cdf X'))+geom_point(aes(x=X, y = cdf.2, color='cdf X =x |Y=y')))
-    }
-    ##################### 
+
   }
-  
-  
+
+
   return(res)
 }
---
+++ R/test.R
@@ -0,0 +1,14 @@
+#testing
+n=400
+
+Z = matrix(runif(2 * n,-1,1), ncol = 2)
+
+X = matrix(runif(2 * n,-1,1), ncol = 2)
+Y = X^2+Z
+
+cond.ind.coef(X,Y,Z)
+
+Y = matrix(rnorm(2*n), ncol = 2)
+
+cond.ind.coef(X,Y,Z)
+
