518755ec4fbb0f0a0e3d2cc50df8b4c1726f5af2
Johannes <hruzaj@student.ethz.ch>
2021-11-18 08:07:32 +0100

    update test to include mmd case

--- R/test.R
+++ R/test.R
@@ -10,8 +10,11 @@ Y = X^2+Z
 
 cond.ind.coef(X,Y)
 cond.ind.coef(X,Y,Z)
+cond.ind.coef(X,Y, method = "mmd")
+cond.ind.coef(X,Y,Z, method = "mmd")
 
 Y = matrix(rnorm(2*n), ncol = 2)
 
 cond.ind.coef(X,Y,Z)
+cond.ind.coef(X,Y,Z, method = "mmd")
 
